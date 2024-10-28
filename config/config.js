8ab95b915db3bd614fca2ae546e13d66cb586d5e
BLyndon <bheil@mail.de>
2024-10-28 06:35:58 +0100

    replace dummy content

--- config/config.js
+++ config/config.js
@@ -26,7 +26,7 @@ export const navigation = {
 
 export const intro = {
   title: "Hey, I'm Benjamin",
-  description: "A Senior Consultant & Java Developer specializing in cloud-native systems, distributed architectures, and event-driven microservices.",
+  description: "A Senior Consultant & Java Developer specializing in cloud-native systems, distributed data-intensive systems, and event-driven microservices.",
   image: profile.src,
   buttons: [
     {
@@ -45,33 +45,33 @@ export const intro = {
 export const about = {
   title: "Who I am",
   description: [
-    "I am a Senior Consultant and Java Developer with a background in theoretical physics, passionate about cloud-native development and distributed systems. My work focuses on building scalable, resilient microservices using Java, Spring Boot, and Kubernetes.",
-    "With extensive experience in event-driven architectures, I leverage Apache Kafka to create real-time data streaming solutions that enable seamless integration and processing. I bring deep expertise in designing and implementing systems that align with digital transformation goals, delivering value through agile development practices.",
+    "I am a Senior Consultant and Java Developer with a background in theoretical physics, passionate about cloud-native development and distributed systems. My work focuses on building microservices using Java, Spring Boot, and Kubernetes",
+    "With a solid foundation in event-driven architectures, I leverage Confluent Kafka to create real-time data streaming solutions that enable seamless integration and processing. I bring deep expertise in designing and implementing data-intensive systems that align with digital transformation goals, delivering value through strong communication and expertise in requirements engineering.",
     "I am actively engaged in the evolving landscape of AI and Large Language Models (LLMs), exploring ways to integrate these cutting-edge technologies into real-world applications to drive innovation and efficiency. Currently, I am excited about expanding my horizons and settling permanently in Barcelona.",
-  ],
+],
 }
 
 export const work = {
   title: "What I do",
   cards: [
     {
-      title: "Cloud Native & Distributed Systems",
-      description: "Designing and implementing cloud-native solutions using Spring Boot, Kubernetes, and ArgoCD, focusing on scalability and resilience.",
+      title: "Cloud Native & GitOps",
+      description: "Designing and implementing cloud-native solutions using Spring Boot, Kubernetes, and ArgoCD, with a focus on GitOps for streamlined deployments.",
       icons: null,
     },
     {
       title: "Event-Driven Architectures with Kafka",
-      description: "Building real-time, event-driven systems with Apache Kafka, enabling efficient data streaming, processing, and integration for complex workflows.",
+      description: "Building event-driven systems with Confluent Kafka, focusing on data integrity and data quality in streaming, processing, and integration for complex workflows.",
+      icons: null,
+    },    
+    {
+      title: "Digital Transformation",
+      description: "Implementing solutions in data-intensive digital transformation projects, managing data migration strategies, and supporting MVP delivery and successful go-live in production environments.",
       icons: null,
     },
     {
-      title: "Digital Transformation Projects",
-      description: "Leading backend development for digital transformation initiatives, deploying microservices on cloud platforms, and driving agile requirements engineering.",
-      icons: null,
-    },
-    {
-      title: "AI & Large Language Models (LLMs)",
-      description: "Exploring and implementing applications of AI and LLMs, focusing on how these technologies can enhance software development and improve business processes.",
+      title: "Analytics & LLMs",
+      description: "Exploring and implementing applications based on LLMs, leveraging classical Machine Learning techniques to provide well-suited solutions for various use cases and challenges.",
       icons: null,
     }
   ],
@@ -81,34 +81,16 @@ export const projects = {
   title: "Projects",
   cards: [
     {
-      title: "ESG/CSRD Reporting",
+      title: "Sustainability Reporting",
       description: "Developed a data integrity framework for a Spring Boot & React-based Kafka connector, ensuring real-time data processing and compliance with ESG standards. Integrated CI/CD pipelines with GitHub Actions and deployed using ArgoCD in Azure.",
-      icons: [
-        {
-          icon: faGithub,
-          link: "https://github.com/BLyndon",
-        },
-      ]
     },
     {
       title: "Digital Transformation",
       description: "Led backend development for microservices deployed on Azure Kubernetes Services, focusing on seamless deployment, scaling, and agile requirements engineering. Utilized Flyway for database migrations and collaborated closely with frontend teams.",
-      icons: [
-        {
-          icon: faGithub,
-          link: "https://github.com/BLyndon",
-        },
-      ]
     },
     {
       title: "Kafka Data Streaming Platform",
       description: "Built a scalable event-driven architecture using Apache Kafka, enabling real-time data processing and analytics. Implemented Kafka Streams for complex joins and transformations, ensuring high throughput and low latency.",
-      icons: [
-        {
-          icon: faGithub,
-          link: "https://github.com/BLyndon",
-        },
-      ]
     },
     {
       title: "Particle Simulation",
@@ -150,7 +132,7 @@ export const SEO = {
 export const links = {
   image: profile.src,
   title: "@BLyndon",
-  description: "Senior Consultant | Java Developer | Cloud Native Enthusiast",
+  description: "Senior Consultant | Java Developer | Cloud Native | Data Enthusiast",
   cards: [
     {
       title: "My GitHub",
@@ -162,7 +144,7 @@ export const links = {
     },
     {
       title: "LinkedIn",
-      link: "https://www.linkedin.com/in/benjamin-heil/",
+      link: "https://www.linkedin.com/in/benjamin-heil-0b02b0197/",
     },
     {
       title: "Amigoscode Course",
